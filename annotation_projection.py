3edfa57dd6cd6cc69b5e8eec414322e2b9feac8f
Iker <igarciaf896@gmail.com>
2022-11-09 17:33:14 -0500

    Fix typo

--- annotation_projection.py
+++ annotation_projection.py
@@ -586,7 +586,7 @@ if __name__ == "__main__":
         "--use_existing_alignments",
         action="store_true",
         help="If set, the script will use the existing alignments in the output directory instead of generating "
-        "new ones. You should use the same --outpur_dir and --output_name as the previous run and the same "
+        "new ones. You should use the same --output_dir and --output_name as the previous run and the same "
         "train, dev and test files.",
     )
 
